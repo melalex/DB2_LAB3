cce39f6d88e6e871bdb10474d79bed4b8aafb758
melalex <melalex490@gmail.com>
2016-09-28 21:18:25 +0300

    Add regex to insert

--- Lab2/model.py
+++ Lab2/model.py
@@ -19,15 +19,24 @@ class Model(object):
         return {key: value for key, value in cls.__dict__.items() if not key.startswith('__') and not callable(key)}
 
     @classmethod
-    def insert(cls, **kwargs):
-        columns = str(kwargs.keys())[1:-1]
-        insert_values = ""
-
+    def __validation(cls, **kwargs):
         for key, value in kwargs:
             value_type = cls.__get_attribute(key)
             if not (value_type and ((type(value) is value_type) or (issubclass(value, Model) and type(value) is int))):
-                raise ValueError
-            insert_values += "%(%s)s," % key
+                return False
+        return True
+
+    @classmethod
+    def insert(cls, **kwargs):
+        if not cls.__validation(**kwargs):
+            raise ValueError
+
+        columns = str(kwargs.keys())[1:-1]
+        insert_values = re.sub(
+            r"(^[\w]+,)|( [\w]+,)|( [\w]$)",
+            lambda match_obj: "%(%s)s," % match_obj.group(0),
+            columns
+        )
 
         insert_values = insert_values[:-1]
         query = "INSERT INTO %s(%s) VALUES(%s)" % cls.__name__, columns, insert_values
